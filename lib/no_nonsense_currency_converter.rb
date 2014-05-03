80a9b925f1bd0d892abd75f1376ec46cb483ffe2
Anshul Mengi <anshulmengi@gmail.com>
2014-05-03 16:16:06 +0530

    Self methods

--- lib/no_nonsense_currency_converter.rb
+++ lib/no_nonsense_currency_converter.rb
@@ -5,18 +5,18 @@ module NoNonsenseCurrencyConverter
   BASE_URL      = 'https://www.google.com/'
   CONVERTER_URL = 'finance/converter'
 
-  def get_converted_currency_value(from, to, amount)
+  def self.get_converted_currency_value(from, to, amount)
     data = get_raw_data(amount, from, to)
     get_final_parsed_amount(data).to_f.round(2)
   end
 
   private
 
-  def get_raw_data(amount, from, to)
+  def self.get_raw_data(amount, from, to)
     open("#{BASE_URL}#{CONVERTER_URL}?a=#{amount}&from=#{from.upcase}&to=#{to.upcase}")
   end
 
-  def get_final_parsed_amount(data)
+  def self.get_final_parsed_amount(data)
     data.read.scan(/<span class=bld>(\d+\.?\d*) [A-Z]{3}<\/span>/).flatten.first
   end
 end
