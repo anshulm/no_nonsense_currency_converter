1236318944440d93fbe4c1c4dc1640820cc22b61
Anshul Mengi <anshulmengi@gmail.com>
2014-05-03 20:07:32 +0530

    Building a conversion dictionary

--- lib/no_nonsense_currency_converter.rb
+++ lib/no_nonsense_currency_converter.rb
@@ -5,9 +5,15 @@ module NoNonsenseCurrencyConverter
   BASE_URL      = 'https://www.google.com/'
   CONVERTER_URL = 'finance/converter'
 
-  def self.get_converted_currency_value(from, to, amount)
-    data = get_raw_data(amount, from, to)
-    get_final_parsed_amount(data).to_f.round(2)
+  @@rate_hash = {}
+
+  def self.get_converted_currency_value(from_currency, to_currency, amount)
+    unless @@rate_hash[from_currency].try(:[], to_currency).try(:[], amount).present?
+      data             = get_raw_data(amount, from_currency, to_currency)
+      converted_amount = get_final_parsed_amount(data).to_f.round(2)
+      @@rate_hash.merge!(from_currency => { to_currency => { amount => converted_amount } })
+    end
+    @@rate_hash[from_currency][to_currency][amount]
   end
 
   private
